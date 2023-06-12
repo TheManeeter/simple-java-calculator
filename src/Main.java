4e77a27d360728f2a8b44a0f5ac64a0ec9b541dd
TheManeeter <maneet.singh863@outlook.com>
2023-06-11 21:39:17 -0400

    Initial Release

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        Scanner scanman = new Scanner(System.in); /* The Scanner Class allows the program to read input from the user */
+        System.out.println("Hello and welcome to the best calculator on the planet!");
+        System.out.println("Please select one of the following operations:");
+        System.out.println("1) Multiply");
+        System.out.println("2) Division");
+        System.out.println("3) Addition");
+        System.out.println("4) Subtraction");
+        int operation = scanman.nextInt();
+        System.out.println("Please enter your first number: ");
+        int FIRST_NUMBER = scanman.nextInt();
+        System.out.println("Please enter your second number: ");
+        int SECOND_NUMBER = scanman.nextInt();
+    int answer;
+        if (operation == 1) { /* MULTIPLICATION */
+           answer = FIRST_NUMBER*SECOND_NUMBER;
+            System.out.println(FIRST_NUMBER+"*"+SECOND_NUMBER+"="+answer);
+        }
+        if (operation == 2) { /* DIVISION */
+            answer = FIRST_NUMBER/SECOND_NUMBER;
+            System.out.println(FIRST_NUMBER+"/"+SECOND_NUMBER+"="+answer);
+        }
+        if (operation == 3) { /* ADDITION */
+            answer = FIRST_NUMBER+SECOND_NUMBER;
+            System.out.println(FIRST_NUMBER+"+"+SECOND_NUMBER+"="+answer);
+        }
+        if (operation == 4) { /* SUBTRACTION */
+            answer = FIRST_NUMBER-SECOND_NUMBER;
+            System.out.println(FIRST_NUMBER+"-"+SECOND_NUMBER+"="+answer);
+        }
+
+        }
+    }
