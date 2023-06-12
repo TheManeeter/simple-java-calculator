41e33916b46d6357b6e399dd649bdc3268573a77
TheManeeter <maneet.singh863@outlook.com>
2023-06-12 18:34:25 -0400

    Initial Release

--- src/Main.java
+++ src/Main.java
@@ -33,6 +33,5 @@ public class Main {
             answer = FIRST_NUMBER-SECOND_NUMBER;
             System.out.println(FIRST_NUMBER+"-"+SECOND_NUMBER+"="+answer);
         }
-        System.out.println("Request Complete");
         }
     }
